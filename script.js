1d2518639c666a8be2e0f1bc6cc299289eb823cd
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2025-10-23 00:31:27 +0000

    feat: tabuleiro dinamico

--- script.js
+++ script.js
@@ -13,12 +13,44 @@ var config = {
     }
 }
 
+function criarTabuleiro(){
+    let tabuleiro = document.createElement("section");
+    tabuleiro.classList.add("Tabuleiro")
+
+    let table = document.createElement("table");
+    tabuleiro.appendChild(table);
+
+    let linha = document.createElement("tr")
+    let posicao = document.createElement("td")
+
+    for (let l = 0; l < 8; l++) {
+        let linha = document.createElement("tr");
+
+        for (let c = 0; c < 8; c++) {
+            let posicao = document.createElement("td");
+            posicao.dataset.linha = l;
+            posicao.dataset.coluna = c;
+
+            linha.appendChild(posicao);
+        }
+
+        table.appendChild(linha);
+    }
+
+    document.getElementById("principal").appendChild(tabuleiro)
+}
+
 
 function criarManipuladorPosicoes(objetoPosicoes){
     objetoPosicoes.forEach(posicao => {
 
-        posicao.addEventListener("click", () => {
-            manipuladorTabuleiro(posicao, config.pecaBranca()) // precisa passar segundo parametro com base na peça que usuario escolher
+        posicao.addEventListener("click", (e) => {
+            manipuladorTabuleiro(posicao, config.pecaBranca(), e) // precisa passar segundo parametro com base na peça que usuario escolher
+        
+        
+        
+        
+        
         })
 
         config.posicoesTabuleiro.push(posicao)
@@ -26,7 +58,9 @@ function criarManipuladorPosicoes(objetoPosicoes){
 }
 
 function inserirPecasTabuleiro(){
+
     const casas = config.posicoesTabuleiro;
+
     // Posiciona as 12 peças pretas nas 3 primeiras linhas
     for (let i = 0; i < 24; i++) {
         // Pega a linha (de 0 a 7) e a coluna (de 0 a 7) a partir do índice
@@ -40,6 +74,7 @@ function inserirPecasTabuleiro(){
             }
         }
     }
+
     // Posiciona as 12 peças brancas nas 3 últimas linhas
     for (let i = 40; i < 64; i++) {
         const linha = Math.floor(i / 8);
@@ -51,12 +86,28 @@ function inserirPecasTabuleiro(){
             }
         }
     }
+
 }
 
-function manipuladorTabuleiro(posicao, corPeca){
-    posicao.appendChild(corPeca);
+function manipuladorTabuleiro(posicaoAtual, corPeca, posicao = null){
+
+    posicaoAtual;
+
+    proximaPosicao;
+
+
+
+    return {
+        peça: (function() {
+            posicaoAtual.appendChild(corPeca);
+        })(),
+        posicaoAtual: posicaoAtual,
+        proximaPosicao: posicao
+    };
 }
 
+criarTabuleiro()
 
 criarManipuladorPosicoes(config.tabuleiro)
+
 inserirPecasTabuleiro()
