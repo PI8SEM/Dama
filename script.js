531171a7fed137064d3d1bb94ca58bed420d6bac
HenriqueUchida <henriqueuchida@hotmail.com>
2025-11-03 22:44:24 -0300

    feat: adicionando turno e logica de movimentação do peao

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 var config = {
+    turno: 'brancas',
     tabuleiro: function(){return document.querySelectorAll("td")},
     posicoesTabuleiro: [],
     pecaPreta: function () {
         let peca = document.createElement('div')
         peca.classList.add('peca', 'preta')
-        peca.dataset.identificador = 'preta'
+        peca.dataset.identificador = 'pretas'
         peca.style.cursor = 'pointer'
+        peca.dataset.tipo = 'peao'
         return peca
     },
     pecaBranca: function() {
         let peca = document.createElement('div')
         peca.classList.add('peca', 'branca')
-        peca.dataset.identificador = 'branca'
+        peca.dataset.identificador = 'brancas'
         peca.style.cursor = 'pointer'
+        peca.dataset.tipo = 'peao'
         return peca
-    }
+    },
+    alternarTurno: function() { //chamar essa função para trocar os turnos.
+        this.turno = (this.turno === 'brancas') ? 'pretas' : 'brancas';
+        document.querySelector('#controle-turno').textContent = this.turno;
+    } 
 }
 
 function criaEventos() {
@@ -23,17 +30,24 @@ function criaEventos() {
         if(event.target.matches('.peca')) {
             let corPeca = event.target.dataset.identificador;
             let elementoCasaPeca = event.target.parentElement;
+            let tipoPeca = event.target.dataset.tipo
             // let linhaPeca = Number(elementoCasaPeca.dataset.linha);
             // let colunaPeca = Number(elementoCasaPeca.dataset.coluna);
-            verificaJogabilidade(corPeca, elementoCasaPeca);
-        } else if(event.target.matches('.casaPreta'))
-            console.log('vc clicou numa casa');
+            if (corPeca === config.turno) {
+                verificaJogabilidade(config.turno, corPeca, elementoCasaPeca, tipoPeca);
+            } else {
+                return
+            }
+        } else if(event.target.matches('.casaPreta, .possivel')){
+            let casaDestino = event.target;
+            manipulaPosicao(casaDestino)
+        }
     })
 }
 
 function criarTabuleiro(){
     let tabuleiro = document.createElement("section");
-    tabuleiro.classList.add("Tabuleiro")
+    tabuleiro.classList.add("tabuleiro")
 
     let table = document.createElement("table");
     tabuleiro.appendChild(table);
@@ -62,20 +76,21 @@ function criarTabuleiro(){
                 if(c%2 != 0) posicao.classList.add("casaBranca")
 
             }
-
-
-
-
         }
 
         table.appendChild(linha);
     }
 
-
-
     document.getElementById("principal").appendChild(tabuleiro)
 }
 
+function resetJogada(){
+    document.querySelectorAll('td').forEach(casa => {
+        casa.classList.remove('possivel');
+        casa.classList.remove('selecionada');
+    });
+    
+}
 
 function inserirPecasTabuleiro(config){
     const casasJogaveis = document.querySelectorAll('.casaPreta');
@@ -91,37 +106,48 @@ function inserirPecasTabuleiro(config){
 
 }
 
-function manipulaPosicao(corPeca, linhaPeca, colunaPeca) {
-     
+function manipulaPosicao(casaDestino) {
+    if(!config.pecaSelecionada) return;
+
+    casaDestino.appendChild(config.pecaSelecionada);
+
+    resetJogada();
+
+    config.alternarTurno();
+
+    config.pecaSelecionada = null;
 }
 
-function verificaJogabilidade(corPeca, casaAtual) {
-    let proximaLinha
+function verificaJogabilidade(turno, corPeca, casaAtual, tipoPeca) {
+    resetJogada();
+    
+    config.pecaSelecionada = casaAtual.querySelector('.peca');
     let linhaAtual = Number(casaAtual.dataset.linha);
     let colunaAtual = Number(casaAtual.dataset.coluna);
     let movPossiveis = [];
-    if (corPeca == 'preta') {
-        proximaLinha = linhaAtual + 1
-
-    } else { 
-        proximaLinha = linhaAtual - 1
-    }
-    //const timesGrupo = classificacaoData.filter(time => time.grupo === element)
-    let elementosProxLinha = document.querySelectorAll(`[data-linha="${proximaLinha}"]`);
-    
-    elementosProxLinha.forEach((e) =>{
-        if(Number(e.dataset.coluna) == colunaAtual +1 || Number(e.dataset.coluna) == colunaAtual -1){
-            (e.hasChildNodes()) && (e.dataset.identificador != corPeca) ? '' : movPossiveis.push(e); //adiciona as casas possíveis na lista
+    if (tipoPeca === 'peao') {    
+        let direcao = (corPeca === 'pretas') ? 1 : -1;
+        
+        let casaEsquerda = document.querySelector(`[data-linha="${linhaAtual + direcao}"][data-coluna="${colunaAtual - 1}"]`);
+        let casaDireita = document.querySelector(`[data-linha="${linhaAtual + direcao}"][data-coluna="${colunaAtual + 1}"]`);
+        
+        if (casaEsquerda && !casaEsquerda.hasChildNodes()) {
+            movPossiveis.push(casaEsquerda);
+            casaAtual.classList.add('selecionada');
+            casaEsquerda.classList.add('possivel');
         }
-    })
-    
-    
+        if (casaDireita && !casaDireita.hasChildNodes()){ 
+            movPossiveis.push(casaDireita);
+            casaAtual.classList.add('selecionada');
+            casaDireita.classList.add('possivel');
+        }}
+
 }
 
 function iniciaJogo() {
-    criaEventos();
     criarTabuleiro();
     inserirPecasTabuleiro(config);
+    criaEventos();
 }
 
 document.addEventListener('DOMContentLoaded', iniciaJogo);
