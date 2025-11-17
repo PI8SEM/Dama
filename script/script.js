43773adcb298e84da0571e42e59254e381a476ac
HenriqueUchida <henriqueuchida@hotmail.com>
2025-11-17 19:31:49 -0300

    feat: equalizando branchs

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
-        let escolha = document.querySelector('.habilitado').addEventListener('click', function(){
-            window.location.href = "./HTML/jogo.html"
-        })
-        
-        document.querySelector('#regrasBtn').addEventListener('click', function(){
-            const dialog = document.querySelector('#dialogRegras');
-            dialog.showModal();  // Abre o diálogo
-        });
+let escolha = document.querySelector('.habilitado').addEventListener('click', function(){
+    window.location.href = "./HTML/jogo.html"
+})
 
-        
-        document.querySelector('#fecharDialog').addEventListener('click', function(){
-            const dialog = document.querySelector('#dialogRegras');
-            dialog.close();  // Fecha o diálogo
-        });
+document.querySelector('#regrasBtn').addEventListener('click', function(){
+    const dialog = document.querySelector('#dialogRegras');
+    dialog.showModal();  // Abre o diálogo
+});
+
+
+document.querySelector('#fecharDialog').addEventListener('click', function(){
+    const dialog = document.querySelector('#dialogRegras');
+    dialog.close();  // Fecha o diálogo
+});
